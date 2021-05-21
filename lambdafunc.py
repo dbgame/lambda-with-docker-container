228bfae631a73b8ae5e622c832c568e9f5d31b25
unhochoi <yms04089@kookmin.ac.kr>
2021-05-21 21:59:19 +0900

    Update lambdafunc.py

--- lambdafunc.py
+++ lambdafunc.py
@@ -2,10 +2,10 @@ import json
 import numpy as np
 import xgboost as xgb
 
-#sp_smdm_xgb_model = xgb.XGBRegressor()
-#bz_smsm_xgb_model = xgb.XGBRegressor()
-#sp_smdm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/sp_smdm_xgb.model')
-#bz_smsm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/bz_smsm_xgb.model')
+sp_smdm_xgb_model = xgb.XGBRegressor()
+bz_smsm_xgb_model = xgb.XGBRegressor()
+sp_smdm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/sp_smdm_xgb.model')
+bz_smsm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/bz_smsm_xgb.model')
 
 def handler(event, context):
     
