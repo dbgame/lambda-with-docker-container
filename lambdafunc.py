a9670bac56cd340b73d3c7455264f13cbbe9886b
unhochoi <yms04089@kookmin.ac.kr>
2021-05-21 20:53:45 +0900

    Update and rename lambda_function.py to lambdafunc.py

--- lambdafunc.py
+++ lambdafunc.py
@@ -8,7 +8,7 @@ bz_smsm_xgb_model = xgb.XGBRegressor()
 sp_smdm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/sp_smdm_xgb.model')
 bz_smsm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/bz_smsm_xgb.model')
 
-def lambda_handler(event, context):
+def handler(event, context):
     
     # TODO implement
     return {
