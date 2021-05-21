32c33a4daf9beb82984f4a5dca254313666c52e0
unhochoi <yms04089@kookmin.ac.kr>
2021-05-21 19:58:03 +0900

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+import xgboost as xgb
+
+# 모델 불러오기
+sp_smdm_xgb_model = xgb.XGBRegressor()
+bz_smsm_xgb_model = xgb.XGBRegressor()
+sp_smdm_xgb_model.load_model('/var/task/sp_smdm_xgb.model')
+bz_smsm_xgb_model.load_model('/var/task/bz_smsm_xgb.model')
+
+def lambda_handler(event, context):
+    
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hi')
+    }
