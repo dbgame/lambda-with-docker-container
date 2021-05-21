6986e1311e5b0cfa2b6a87586be24ab06d5f6e63
unhochoi <yms04089@kookmin.ac.kr>
2021-05-21 21:22:36 +0900

    Update lambdafunc.py

--- lambdafunc.py
+++ lambdafunc.py
@@ -2,7 +2,6 @@ import json
 import numpy as np
 import xgboost as xgb
 
-# 모델 불러오기
 sp_smdm_xgb_model = xgb.XGBRegressor()
 bz_smsm_xgb_model = xgb.XGBRegressor()
 sp_smdm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/sp_smdm_xgb.model')
