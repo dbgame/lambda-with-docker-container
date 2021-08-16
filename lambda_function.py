6be7127e075a83b7e34899a429ffd796d6658f50
unhochoi <yms04089@kookmin.ac.kr>
2021-08-16 13:57:58 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,12 @@
+# package import
 import json
 import numpy as np
-import xgboost as xgb
+import tensorflow as tf
+import tensorflow as tf
 
-sp_smdm_xgb_model = xgb.XGBRegressor()
-bz_smsm_xgb_model = xgb.XGBRegressor()
-sp_smdm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/sp_smdm_xgb.model')
-bz_smsm_xgb_model.load_model('/var/task/lambda-with-docker-container/model/bz_smsm_xgb.model')
+# Model Load 
+sp_smdm_dnn_model = tf.keras.models.load_model('/var/task/lambda-with-docker-container/model/dnn_sp_smdm_model')
+bz_smsm_dnn_model = tf.keras.models.load_model('/var/task/lambda-with-docker-container/model/dnn_bz_smsm_model')
 
 def handler(event, context):
     
