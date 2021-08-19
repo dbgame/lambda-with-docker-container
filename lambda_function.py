e0d0b0052a2182f9d540752d4c667c9f7065c196
unhochoi <yms04089@kookmin.ac.kr>
2021-08-19 14:56:22 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,26 +1,14 @@
-import time
-
-package_start_ms = int(round(time.time() * 1000))
-
 # package import
 import json
 import numpy as np
 import tensorflow as tf
 
-package_end_ms = int(round(time.time() * 1000))
-
-model_load_start_ms = int(round(time.time() * 1000))
-
 # Model Load 
 sp_smdm_dnn_model = tf.keras.models.load_model('/var/task/lambda-with-docker-container/model/dnn_sp_smdm_model')
 bz_smsm_dnn_model = tf.keras.models.load_model('/var/task/lambda-with-docker-container/model/dnn_bz_smsm_model')
 
-model_load_end_ms = int(round(time.time() * 1000))
-
 def handler(event, context):
     
-    function_start_ms = int(round(time.time() * 1000))
-
     body = event["body-json"]
 
     # event 로부터 feature 전처리
@@ -34,20 +22,25 @@ def handler(event, context):
 
     # 모델 입력으로 사용할 input_feature 생성
     input_feature = np.array([[lr,lc,rc,ld,rd,lnnz,rnnz]])
-
+    
     # input_feature 에 대한 모델별 예측값 생성
     sp_smdm_dnn_result = sp_smdm_dnn_model.predict(input_feature)
     bz_smsm_dnn_result = bz_smsm_dnn_model.predict(input_feature)
-
-    function_end_ms = int(round(time.time() * 1000))
-
-    latency_result = \
-        "package import : " + str(package_end_ms - package_start_ms) + " , " + \
-        "model load: " + str(model_load_end_ms - model_load_start_ms) + " , " + \
-        "function latency : " + str(function_end_ms - function_start_ms)
     
-    # 결과 반환
+    # sp_smdm 이 최적일 경우
+    if (sp_smdm_dnn_result[0] <= bz_smsm_dnn_result[0]):
+        optim_method = "sp_smdm"
+    # bz_smsm 이 최적일 경우
+    else:
+        optim_method = "bz_smsm"
+    
+	# 결과 생성
+    result = "sp_smdm : " + str(sp_smdm_dnn_result[0]) + " , " + \
+		"bz_smsm : " + str(bz_smsm_dnn_result[0]) + " , " + \
+		"optim_method : " + optim_method
+
+	# 결과 반환
     return {
         'statusCode': 200,
-        'body': json.dumps(latency_result)
+        'body': json.dumps(result)
     }
