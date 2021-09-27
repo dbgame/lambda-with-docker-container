07a29491cdb0fc9a64d63389b8ecaac26f068817
unhochoi <yms04089@kookmin.ac.kr>
2021-09-27 15:35:43 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,10 +2,15 @@
 import json
 import numpy as np
 import tensorflow as tf
+from pickle
+from sklearn.preprocessing import MinMaxScaler
 
 # Model Load 
 bz_smsm_dnn_model = tf.keras.models.load_model('/var/task/lambda-with-docker-container/model/dnn_bz_smsm_model')
 sp_smdm_dnn_model = tf.keras.models.load_model('/var/task/lambda-with-docker-container/model/dnn_sp_smdm_model')
+
+# Scaler Load
+minmax_scaler = pickle.load('/var/task/lambda-with-docker-container/model/minmax_scaler.pkl','rb')
 
 def handler(event, context):
     
@@ -23,9 +28,12 @@ def handler(event, context):
     # 모델 입력으로 사용할 input_feature 생성
     input_feature = np.array([[lr,lc,rc,ld,rd,lnnz,rnnz]])
     
+    # input_feature 에 minmax scaler 적용
+    input_feature_scaler = minmax_scaler.transform(input_feature)
+
     # input_feature 에 대한 모델별 예측값 생성
-    bz_smsm_dnn_result = bz_smsm_dnn_model.predict(input_feature)
-    sp_smdm_dnn_result = sp_smdm_dnn_model.predict(input_feature)
+    bz_smsm_dnn_result = bz_smsm_dnn_model.predict(input_feature_scaler)
+    sp_smdm_dnn_result = sp_smdm_dnn_model.predict(input_feature_scaler)
 
     # sp_smdm 이 최적일 경우
     if (sp_smdm_dnn_result[0] <= bz_smsm_dnn_result[0]):
